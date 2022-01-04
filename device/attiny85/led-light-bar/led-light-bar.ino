acefedf42ca0d1cd478190e9e1926068252bf103
darkowl91 <darkowl91@gmail.com>
2022-01-04 21:58:05 +0300

    setup

--- device/attiny85/led-light-bar/led-light-bar.ino
+++ device/attiny85/led-light-bar/led-light-bar.ino
@@ -1,6 +1,6 @@
 /*
     Smart LED Controller based on ATTINY85 Digispark USB
-    Board Manager:
+    Board Manager: https://raw.githubusercontent.com/ArminJo/DigistumpArduino/master/package_digistump_index.json
 
     PINS:
     * P0 - Photo-resistor input
@@ -8,12 +8,23 @@
     * P2 - Button input
 */
 
-#include <Arduino.h>
+#include <Adafruit_NeoPixel.h>
 
-void setup()
-{
+#define BUTTON_PIN          2
+#define LED_PIN             1
+#define LED_COUNT           7
+#define PHOTO_RESISTOR_PIN  0
+
+Adafruit_NeoPixel led_bar = Adafruit_NeoPixel(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+
+void setup() {
+
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(PHOTO_RESISTOR_PIN, INPUT);
+
+  led_bar.begin();
 }
 
-void loop()
-{
+void loop() {
 }
