34d8f1d2f37614565bbd9e9e34596a3349e787ed
darkowl91 <darkowl91@gmail.com>
2022-01-05 01:44:40 +0300

    added fill_rainbow

--- device/attiny85/led-light-bar/led-light-bar.ino
+++ device/attiny85/led-light-bar/led-light-bar.ino
@@ -8,23 +8,52 @@
     * P2 - Button input
 */
 
-#include <Adafruit_NeoPixel.h>
+#include <OneButton.h>
+#include <FastLED.h>
 
 #define BUTTON_PIN          2
 #define LED_PIN             1
-#define LED_COUNT           7
+#define NUM_LEDS            7
 #define PHOTO_RESISTOR_PIN  0
+#define LED_TYPE            WS2812
 
-Adafruit_NeoPixel led_bar = Adafruit_NeoPixel(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+OneButton button(BUTTON_PIN, false);
+CRGB leds[NUM_LEDS];
 
-void setup() {
 
+void setup()
+{
   pinMode(BUTTON_PIN, INPUT_PULLUP);
   pinMode(LED_PIN, OUTPUT);
   pinMode(PHOTO_RESISTOR_PIN, INPUT);
 
-  led_bar.begin();
+  button.attachClick(buttonClick);
+  button.attachDoubleClick(buttonDoubleClick);
+
+  FastLED.addLeds<LED_TYPE, LED_PIN, GRB>(leds, NUM_LEDS);
+  FastLED.setBrightness(getBrightness());
 }
 
-void loop() {
+void loop()
+{
+  button.tick();
+}
+
+int getBrightness()
+{
+  int resistor_value = analogRead(PHOTO_RESISTOR_PIN);
+  return map(resistor_value, 0, 1023, 0, 255);
+}
+
+void buttonClick()
+{
+  FastLED.setBrightness(getBrightness());
+  fill_rainbow(leds, NUM_LEDS, 0, 40);
+  FastLED.show();
+}
+
+// Turn off the LED
+void buttonDoubleClick()
+{
+  FastLED.clear(true);
 }
